8e30a04f0c071bbca32744b735abd559935a6442
Tito <50543732+DevWebJr@users.noreply.github.com>
2023-01-15 21:20:07 +0100

    ajout de la fonction addSnap.

--- src/app/components/face-snap/face-snap.component.ts
+++ src/app/components/face-snap/face-snap.component.ts
@@ -10,14 +10,30 @@ export class FaceSnapComponent implements OnInit{
   description!: string;
   createdDate!: Date; 
   snaps!: number;
+  buttonText!: string;
   imageUrl!: string;
+  snapped!: boolean;
 
   ngOnInit(){
     this.title = "Développeur Web";
     this.description = "Parce que j'aime créer des liens..."
     this.createdDate = new Date();
     this.snaps = 6;
+    this.buttonText = "Snap"
     this.imageUrl = "https://www.portices.fr/wp-content/uploads/2020/11/developpeur-web-mobile-formation.jpg";
+    this.snapped = false;
   }
 
+  addSnap() {
+    if(!this.snapped) {
+      this.snaps++;
+      this.snapped = true;
+      this.buttonText = "Snap";
+    }
+    else {
+      this.snaps--;
+      this.snapped = false;
+      this.buttonText = "Unsnap";
+    }
+  }
 }
